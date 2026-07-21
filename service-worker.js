a24d0fbad7c873e8594ae510ef74dcd6f7e75cd7
yanvarr-source <yanvarr@icloud.com>
2026-07-21 07:06:32 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "pyatnashki-v1";
+
+const urlsToCache = [
+  "./",
+  "./pyatnashki_safari.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
